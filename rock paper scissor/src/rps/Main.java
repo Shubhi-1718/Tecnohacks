97b259f8d21b9785008c55bd22f6f79389088503
Shubhangini Agrawal <subhinikunj@gmail.com>
2023-09-07 16:59:12 +0530

    Initial commit

---
+++ rock paper scissor/src/rps/Main.java
@@ -0,0 +1,8 @@
+package rps;
+
+public class Main 
+{
+public static void main(String[] args) {
+FrontPage.front();
+}
+}
---
+++ tic tac toe/src/ttt/Main.java
@@ -0,0 +1,9 @@
+package ttt;
+
+public class Main 
+{
+	public static void main(String args[])
+	{
+		TicTacToe ttto = new TicTacToe();
+	}
+}
